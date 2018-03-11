887e0f4c1d26b5db18d062515f839293f706af13
Andy Luu <andyl2020@yahoo.ca>
2018-03-10 21:13:10 -0800

    added ardino serial file

---
+++ Data processor (andy and melvin)/Andy's SDK app/ZIPPED - Microsoft Band Sensors/C#/arduinotoserial1.1/arduinotoserial1.1.cpp
@@ -0,0 +1,11 @@
+// arduinotoserial1.1.cpp : main project file.
+
+#include "stdafx.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    Console::WriteLine(L"Hello World");
+    return 0;
+}
